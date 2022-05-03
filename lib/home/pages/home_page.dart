0474f9219eb13e4184620577433b3e1d3806d7ea
Bartosz Tomaszewski <bartosztom3@wp.pl>
2022-05-03 20:36:13 +0200

    Build simple UI

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../calendar/pages/calendar_page.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({
     Key? key,
@@ -8,8 +10,91 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Catch'em ideas"),
+      backgroundColor: const Color.fromARGB(224, 140, 115, 207),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 15, top: 10),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Catch'em ideas",
+                          style: TextStyle(
+                              color: Color(0xFF00003f),
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 12),
+                          child: ElevatedButton(
+                            onPressed: null,
+                            child: Text('Quick'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 15, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Manage your thoughts',
+                      style: TextStyle(
+                          color: Color(0xFF00003f),
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const CalendarPage()),
+                          );
+                        },
+                        icon: const Icon(Icons.calendar_today)),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 18.0),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30)),
+                        boxShadow: [BoxShadow(blurRadius: 10.0)],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
