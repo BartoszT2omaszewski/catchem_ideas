db4adaee2f143ff7e43b90bc3b09c497b60fb205
Bartosz Tomaszewski <bartosztom3@wp.pl>
2022-05-04 12:23:20 +0200

    Add basic note for ideas

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -88,14 +88,54 @@ class HomePage extends StatelessWidget {
                             topRight: Radius.circular(30)),
                         boxShadow: [BoxShadow(blurRadius: 10.0)],
                       ),
+                      child: GridView(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          children: const [
+                            IdeaWidget('1'),
+                            IdeaWidget('2'),
+                            IdeaWidget('3'),
+                          ]),
                     ),
                   )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class IdeaWidget extends StatelessWidget {
+  const IdeaWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(
+          top: 15,
+          left: 15,
+          right: 15,
+        ),
+        height: 200,
+        width: 250,
+        decoration: const BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text(title)],
+        ));
   }
 }
