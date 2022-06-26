3b7043a7efaa6c5970d708799c1f1cd2a1a0d062
Bartosz Tomaszewski <bartosztom3@wp.pl>
2022-06-27 00:12:25 +0200

    Automate injection registering

--- lib/app/features/repositories/authors_repository.dart
+++ lib/app/features/repositories/authors_repository.dart
@@ -1,8 +1,6 @@
 import 'package:catchem_ideas/app/features/models/author_model.dart';
 import 'package:catchem_ideas/data/remote_data_sources/authors_remote_data_source.dart';
-import 'package:injectable/injectable.dart';
 
-@injectable
 class AuthorsRepository {
   AuthorsRepository({required this.remoteDataSource});
 
